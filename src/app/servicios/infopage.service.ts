8e9d95f61f4def9da78dc88135351bababec1cab
Daniel Castiblanco <razzziel001@gmail.com>
2020-01-05 21:52:22 -0500

    Extraemos de una base de datos externa la info de la web en un json

--- src/app/servicios/infopage.service.ts
+++ src/app/servicios/infopage.service.ts
@@ -8,15 +8,28 @@ import { InfoWeb } from "../interfaces/info-web";
 export class InfopageService {
   info: InfoWeb = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) {
-    console.log("Servicio de infoPagina listo");
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+  private cargarInfo() {
     //leer el archovo json
     this.http.get("assets/data/data-pagina.json").subscribe(resp => {
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
     });
+  }
+  //Extraemos de una base de datos externa la info de la web en un json
+  private cargarEquipo() {
+    this.http
+      .get("https://cursoangu.firebaseio.com/equipo.json")
+      .subscribe((resp: any[]) => {
+        this.equipo = resp;
+
+        console.log(resp);
+      });
   }
 }
